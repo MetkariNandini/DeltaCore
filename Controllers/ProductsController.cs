ce57dce0fe2c4d623a5b969da120a0515cfa9856
Nandini Metkari <nandinimetkari0498@gmail.com>
2022-07-03 09:02:33 +0530

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using DeltaCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeltaCore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<ProductsService> productlist = new List<ProductsService>();
+            ProductsService products = new ProductsService();
+            productlist.Add(new ProductsService
+            {
+                ProductID = 1001,
+                Name = "Tools Gears",
+                Description = "Gear tools production specification"
+            });
+            productlist.Add(new ProductsService
+            {
+                ProductID = 2001,
+                Name = "Panels",
+                Description = "Switch Gear Panels"
+            });
+            productlist.Add(new ProductsService
+            {
+                ProductID = 2008,
+                Name = "DocTonar",
+                Description = "Document Solar Objects"
+            });
+            return View(productlist);
+        }
+    }
+}
